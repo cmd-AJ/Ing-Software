a4a462f9de1f234557822a9ec09c8d65882a1b5b
wwIrvingww <mor22781@guvg.edu.gt>
2024-03-30 21:14:28 -0600

    DropDown menu added

--- Scrum/Working/src/pages/Login.tsx
+++ Scrum/Working/src/pages/Login.tsx
@@ -1,6 +1,15 @@
-import { IonContent, IonPage, IonInput, IonButton } from '@ionic/react';
-import './Login.css';
 import React from 'react';
+import {
+    IonContent,
+    IonPage,
+    IonInput,
+    IonButton,
+    IonItem,
+    IonSelect,
+    IonSelectOption,
+    IonList
+} from '@ionic/react';
+import './Login.css';
 
 const Login = () => {
     return (
@@ -9,10 +18,21 @@ const Login = () => {
                 <div className='login'>
                     <span className="loginTitle">Login</span>
                     <form className='loginForm'>
+
+
                         <label>Email</label>
                         <IonInput type="text" className="loginInput" placeholder='Enter your email...' />
                         <label>Password</label>
-                        <IonInput type="password" className="loginInput" placeholder='Enter your password'/>
+                        <IonInput type="password" className="loginInput" placeholder='Enter your password' />
+                        <label>Role</label>
+                        <IonList className="RoleButton" color="none">
+                            <IonItem>
+                                <IonSelect placeholder='Select your Role'>
+                                    <IonSelectOption value="recruiter">Recruiter</IonSelectOption>
+                                    <IonSelectOption value="worker">Worker</IonSelectOption>
+                                </IonSelect>
+                            </IonItem>
+                        </IonList>
                         <IonButton expand="block" className='loginButton' color="none">Login</IonButton>
                     </form>
                     <IonButton expand="block" className='loginRegisterButton' color="none">Register</IonButton>
