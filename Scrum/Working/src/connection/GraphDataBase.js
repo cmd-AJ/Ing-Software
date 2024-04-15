d101d652bc4c41f9fdaa346d19369eb814cf269a
chuy-zip <andreschuy03@gmail.com>
2024-04-14 18:53:46 -0600

    added first version of connection

---
+++ Scrum/Working/src/connection/GraphDataBase.js
@@ -0,0 +1,16 @@
+import neo4j from 'neo4j-driver';
+
+var graphDB = neo4j
+
+const URI = 'neo4j+s://3805d5ba.databases.neo4j.io:7687'
+const user = 'neo4j'
+const password = 'm2FnqjxJjrcve7-tqOVCmzgggmB9gKg_g13oOaB2JPw'
+let driver
+
+try {
+    driver = graphDB.driver(URI,neo4j.auth.basic(user, password))
+} catch (error) {
+    console.log(`Connection error\n${err}\nCause: ${err.cause}`)
+}
+
+export default driver;
