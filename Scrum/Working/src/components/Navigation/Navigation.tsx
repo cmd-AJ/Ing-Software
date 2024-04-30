3dd33f72c635fd33169b754218e9b0d66246e73b
josuemj <josuemj456@gmail.com>
2024-04-29 20:53:14 -0600

    feat!: Single page implemented, pending adding carrousel to serched.tsx or creating new page

--- Scrum/Working/src/components/Navigation/Navigation.tsx
+++ Scrum/Working/src/components/Navigation/Navigation.tsx
@@ -8,15 +8,17 @@ import {
   IonIcon,
   IonText,
 } from '@ionic/react';
-import { personOutline, mailOutline, briefcaseOutline, peopleOutline, settingsOutline } from 'ionicons/icons';
+import { useHistory } from 'react-router-dom'; // Import useHistory
+import { personOutline, settingsOutline } from 'ionicons/icons';
 import SearchBar from '../Search/SearchBar';
-import './Navigation.css'
+import './Navigation.css';
 
 interface NavigationBarProps {
   setRequest: (value: string) => void;
 }
 
 const NavigationBar: React.FC<NavigationBarProps> = ({ setRequest }) => {
+  const history = useHistory(); // Initialize useHistory hook
 
   const handleRequestChange = (value: string) => {
     if (value.trim() !== '') {
@@ -32,7 +34,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ setRequest }) => {
       <IonToolbar color="primary">
         <IonGrid>
           <IonRow className="ion-align-items-center">
-            <IonCol className="ion-text-center">
+            <IonCol className="ion-text-center" onClick={() => history.push('/searched')}>
               <IonText className="appName-text">SABTE</IonText>
             </IonCol>
             <IonCol size="6">
@@ -47,13 +49,17 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ setRequest }) => {
             <IonCol className="ion-text-center">
               <IonText className="custom-text">AGENDA</IonText>
             </IonCol>
-            <IonCol className="ion-text-center"><IonIcon icon={personOutline} className="navbar-icon" /></IonCol>
-            <IonCol className="ion-text-center"><IonIcon icon={settingsOutline} className="navbar-icon" /></IonCol>
+            <IonCol className="ion-text-center" onClick={() => history.push('/empleado')}>
+              <IonIcon icon={personOutline} className="navbar-icon" />
+            </IonCol>
+            <IonCol className="ion-text-center">
+              <IonIcon icon={settingsOutline} className="navbar-icon" />
+            </IonCol>
           </IonRow>
         </IonGrid>
       </IonToolbar>
     </IonHeader>
   );
 };
-  
+
 export default NavigationBar;
