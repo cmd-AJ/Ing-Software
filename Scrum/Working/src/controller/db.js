c5e991dc87fa0483221d726c8e535f6e3150b3ef
chuy-zip <andreschuy03@gmail.com>
2024-05-03 17:15:33 -0600

    Created trustNetworka and function to get it

--- Scrum/Working/src/controller/db.js
+++ Scrum/Working/src/controller/db.js
@@ -68,7 +68,7 @@ export async function getWorkers(trabajo) {
 
         // Transformar los registros obtenidos en un arreglo de objetos JSON
         const workers = result.records.map(record => {
-            const worker = record.get('tra');
+            const worker = record.get('usr');
             return {
                 nombre: worker.properties.Nombre, // Obtener el nombre del trabajo
                 telefono: worker.properties.Telefono,
@@ -163,6 +163,36 @@ export async function getContactsByUserDPI(dpi) {
         console.error('Error getting contacts by user DPI:', error);
         throw error;
     }
+}
+
+
+export async function getTrustedUsersByDpi(dpi){
+    const session = createSession();
+
+    try {
+        const query = `MATCH p=(usr1:Usuario {DPI: '${dpi}'})-[:confia_en]->(usr2:Usuario) RETURN usr2 ORDER BY usr2.Rating DESC`;
+        const result = await session.run(query);
+
+        // Transformar los registros obtenidos en un arreglo de objetos JSON
+        const users = result.records.map(record => {
+            const user = record.get('usr2');
+            return {
+                nombre: user.properties.Nombre, // Obtener el nombre del trabajo
+                telefono: user.properties.Telefono,
+                municipio: user.properties.Municipio,
+                rating: user.properties.Rating,
+                apellido: user.properties.Apellido,
+                dpi: user.properties.DPI
+            };
+        });
+        return users;
+    } catch (error) {
+        console.error('Error getting trusted users:', error);
+        throw error;
+    } finally {
+        await session.close();
+    }
+
 }
 
 
