dbc2d4f23586dddc70c7afec70b533da92b5ffca
josuemj <josuemj456@gmail.com>
2024-05-05 20:49:19 -0600

    feat: about section completed

---
+++ Scrum/Working/src/components/About/Explain.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonImg,
+} from "@ionic/react";
+import "./Explain.css";
+
+interface InfoCardProps {
+  title: string;
+  content: string;
+  imageUrl: string;
+}
+
+const InfoCard: React.FC<InfoCardProps> = ({ title, content, imageUrl }) => {
+    const imageStyle = {
+        backgroundImage: `url(${imageUrl})`
+      };
+  return (
+    <div className="info-content">
+      <div className="content-title">
+        <h2 className="text-title">{title}</h2>
+        <p className="content-style">{content}</p>
+      </div>
+      <div className="circular-image-wrapper">
+      <div className="image-holder" style={imageStyle}></div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
--- Scrum/Working/src/pages/About.tsx
+++ Scrum/Working/src/pages/About.tsx
@@ -14,6 +14,7 @@ import {
   IonCardTitle,
   IonCardContent,
 } from "@ionic/react";
+import InfoCard from "../components/About/Explain";
 import "./About.css";
 
 const About: React.FC = () => {
@@ -64,7 +65,26 @@ const About: React.FC = () => {
             </IonCardContent>
           </section>
           <section id="nextSection">
-                <h1 className="emptyTitle">Next Section Title</h1>
+                <h1 className="emptyTitle">
+                  Sistema Accesible<br/>de busqueda de trabajos especificos
+                </h1>
+          </section>
+            <InfoCard
+              title="Misión"
+              content="En el corazón de nuestra misión en SABTE se encuentra el compromiso de transformar y optimizar la manera en que los profesionales especializados y los empleadores se conectan en el mercado laboral. Nuestra visión es desarrollar una plataforma digital innovadora que no solo facilite un encuentro eficiente entre la oferta y demanda de servicios especializados, sino que también promueva un entorno de confianza y transparencia. Aspiramos a eliminar las barreras que actualmente dificultan la contratación de talentos especializados, ofreciendo una solución accesible e integral que responda a las necesidades de ambos sectores.
+
+              "
+              imageUrl="https://procesa.es/wp-content/uploads/2019/12/bsqueda-empleo.jpg"
+            />
+            <InfoCard
+              title="Visión"
+
+              content="Nuestra visión se extiende a construir una comunidad en donde cada miembro, sin importar el nivel de habilidades tecnológicas, pueda encontrar oportunidades de crecimiento y desarrollo profesional. Estamos dedicados a hacer que cada interacción en nuestra plataforma no solo sea posible, sino excepcional, asegurando que cada profesional pueda demostrar su valía y cada empleador pueda hallar el talento ideal. En SABTE, creemos en el poder de unir a las personas a través de la tecnología para crear un futuro más inclusivo y próspero para todos los profesionales especializados.
+              "
+              imageUrl="https://c8.alamy.com/compes/2cb921p/adicta-a-la-tecnologia-joven-carrera-mixta-femenino-hombre-personas-utilizar-smartphones-chat-en-redes-sociales-comunicarse-en-linea-tipo-enviar-mensajes-de-correo-electronico-concepto-de-vida-virtual-dibujos-animados-vector-ilustracion-2cb921p.jpg"
+            />
+          <section>
+            
           </section>
         </section>
 
