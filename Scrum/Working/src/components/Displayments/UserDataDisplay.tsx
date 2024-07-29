963631f6ab44be2608d83d5a440986837621e3bd
Jose-Prince <joseprince148@gmail.com>
2024-07-29 16:44:15 -0600

    Feat: Jobs done title

--- Scrum/Working/src/components/Displayments/UserDataDisplay.tsx
+++ Scrum/Working/src/components/Displayments/UserDataDisplay.tsx
@@ -1,6 +1,7 @@
 import VerticalDivider from "../Dividers/VerticalDivider";
 import VerticalDisplay from "../Dividers/VerticalDivider";
 import ContratsDisplay from "./ContratsDisplay";
+import JobsDisplay from "./JobsDisplay";
 
 interface ContainerProps {
 
@@ -11,7 +12,7 @@ const UserDataDisplay: React.FC<ContainerProps> = () => {
         <div style={{display: 'flex', height: '100%', minHeight: '378px'}}>
             <ContratsDisplay/>
             <VerticalDivider />
-            <div></div>
+            <JobsDisplay />
         </div>
     )
 }
