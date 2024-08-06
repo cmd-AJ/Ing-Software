2bad092dbc3b1d317a700ac18edea1a1ea41ede2
josuemj <josuemj456@gmail.com>
2024-08-06 15:19:02 -0600

    test: about.tsc passed

---
+++ Scrum/Working/src/controller/tests/About.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '../../pages/About';
+
+test('renders About component', () => {
+  render(<About />);
+});
