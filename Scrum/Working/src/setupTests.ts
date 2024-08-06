6eec7615bd435fea437d5a87afe7dcd88201eacf
josuemj <josuemj456@gmail.com>
2024-08-06 13:00:25 -0600

    test: ExplorerContainer front-end test

--- Scrum/Working/src/setupTests.ts
+++ Scrum/Working/src/setupTests.ts
@@ -3,6 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
+
 
 // Mock matchmedia
 window.matchMedia = window.matchMedia || function() {
--- Scrum/Working/vite.config.ts
+++ Scrum/Working/vite.config.ts
@@ -1,6 +1,8 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +14,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude, 'node_modules/**']
   }
 })
