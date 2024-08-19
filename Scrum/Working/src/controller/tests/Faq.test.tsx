e3878d5366081ff1b292f7fcb2cb01229851d0b6
cmd-AJ <Andrewlol774@gmail.com>
2024-08-18 19:58:34 -0600

    Corregiendo el test de FAQS

--- Scrum/Working/src/controller/tests/Faq.test.tsx
+++ Scrum/Working/src/controller/tests/Faq.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Faq from '../../components/Help/Faq';
 
 test('renders Faq component', () => {
   render(<Faq />);
-  expect(screen.getByText('Preguntas Frecuentes')).toBeInTheDocument();
+  expect(screen.getByText('Product Info')).toBeInTheDocument();
+  expect(screen.getByText('¿Puedo desactivar mi cuenta?')).toBeInTheDocument();
 });
