8edebd4350773ee0ed9e87dd4f51c974803133d5
josuemj <98498772+josuemj@users.noreply.github.com>
2024-08-28 18:40:38 -0600

    feat: threads space created

--- Scrum/Working/src/pages/threads/Threads.tsx
+++ Scrum/Working/src/pages/threads/Threads.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { IonPage } from '@ionic/react';
+import './Threads.css'
 
 const Threads: React.FC = () => {
     return (
         <IonPage>
-            
+            <div className='threads-space'>
+                
+            </div>
         </IonPage>
     )
 }
