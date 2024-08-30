24b28a74985a5bd5dbd9544f859cff2c5c10a726
josuemj <98498772+josuemj@users.noreply.github.com>
2024-08-29 19:07:41 -0600

    sketching post text field on goin task

--- Scrum/Working/src/components/Threads/PostField.tsx
+++ Scrum/Working/src/components/Threads/PostField.tsx
@@ -1,37 +1,65 @@
 import React, { useState } from 'react';
-import { IonButton, IonInput, IonItem, IonLabel, IonTextarea } from '@ionic/react';
+import { IonButton, IonItem, IonLabel, IonTextarea, IonIcon } from '@ionic/react';
+import { camera } from 'ionicons/icons';
 import './PostField.css';
 
-
 const PostField: React.FC = () => {
-    const [inputText, setInputText] = useState('');
-  
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      console.log('Posted:', inputText);
-      setInputText('');
-    };
-  
-    return (
-      <div className='post-field-container'>
-        <div className='t-profile-container'></div>
-        <form onSubmit={handleSubmit}>
-          <IonItem>
-            <IonLabel position="stacked">Write something</IonLabel>
-            <IonTextarea
-              value={inputText}
-              onIonChange={(e) => setInputText(e.detail.value!)}
-              placeholder="Type your message..."
-              autoGrow={true}
-            />
-          </IonItem>
+  const [inputText, setInputText] = useState('');
+  const [photo, setPhoto] = useState<File | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Posted:', inputText);
+    if (photo) {
+      console.log('Photo:', photo.name);
+    }
+    setInputText('');
+    setPhoto(null);
+  };
+
+  const handlePhotoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setPhoto(event.target.files[0]);
+    }
+  };
+
+  return (
+    <div className='post-field-container'>
+      <div className='t-profile-container'></div>
+      <form onSubmit={handleSubmit} className="form-container">
+        <IonItem className="input-container">
+          <IonIcon
+            icon={camera}
+            slot="start"
+            onClick={() => document.getElementById('uploadPhoto')?.click()}
+            className="photo-icon"
+          />
+          <IonLabel position="stacked">Postea</IonLabel>
+          <IonTextarea
+            value={inputText}
+            onIonChange={(e) => setInputText(e.detail.value!)}
+            placeholder="Escribe algo..."
+            autoGrow={true}
+            className="textarea-input"
+          />
+          
+        </IonItem>
+        <div className="action-container">
           <IonButton expand="full" type="submit" className="post-button">
             Post
-          </IonButton>
-        </form>
-      </div>
-    );
-  };
-  
-  export default PostField;
-  
+            </IonButton>
+          <input
+            type="file"
+            id="uploadPhoto"
+            accept="image/*"
+            style={{ display: 'none' }}
+            onChange={handlePhotoChange}
+          />
+          
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PostField;
--- Scrum/Working/src/pages/threads/Threads.tsx
+++ Scrum/Working/src/pages/threads/Threads.tsx
@@ -9,6 +9,7 @@ const Threads: React.FC = () => {
             <div className='threads-space'>
                 <div className="threads-container">
                     {/* TO DO  ALL thread components*/}
+                    <PostField/>
                 </div>
             </div>
         </IonPage>
