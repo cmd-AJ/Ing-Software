5e9c5d9aa36999f0d853890961fb0944c69f2fd5
chuy-zip <andreschuy03@gmail.com>
2024-09-19 13:38:05 -0600

    made function to create post

--- Scrum/Working/src/controller/ThreadController.tsx
+++ Scrum/Working/src/controller/ThreadController.tsx
@@ -1,30 +1,34 @@
-export async function createThreadPost(dpi1: string, dpi2: string) {
+export async function createThreadPost(user_dpi: string, post_text: string, post_Image: string) {
 
-    const chatExists = true;
+    //Create new chat calling the endpoint /contacts/createChat
+    try {
+        const data = {
+            "dpiUser": user_dpi,
+            "postText": post_text,
+            "postImage": post_Image
+          };
 
-    if (!chatExists) {
-        //Create new chat calling the endpoint /contacts/createChat
-        try {
-            const data = {
-                dpi1: dpi1,
-                dpi2: dpi2
-            };
+        const response = await fetch(`http://${import.meta.env.VITE_API_HOSTI}:${import.meta.env.VITE_PORTI}/threads/createPost`, {
+            method: 'POST',
+            headers: {
+                'api-key': import.meta.env.VITE_API_KEY,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
 
-            const response = await fetch(`http://${import.meta.env.VITE_API_HOSTI}:${import.meta.env.VITE_PORTI}/contacts/createChat`, {
-                method: 'POST',
-                headers: {
-                    'api-key': import.meta.env.VITE_API_KEY,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
+        console.log(response)
+        
 
-            if (!response.ok) {
-                throw new Error('Failed to create chat');
-            }
-        } catch (error) {
-            console.error("Error while creating chat:", error);
-            throw error; // Rethrow the error to be handled by the caller
+        if (!response.ok) {
+            throw new Error('Failed to create new post');
         }
+
+        return response.json()
+
+    } catch (error) {
+        console.error("Error while creating new thread post:", error);
+        throw error; 
     }
+
 }
