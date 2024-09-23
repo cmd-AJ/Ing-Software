e38559d9960b18dcaa458b99b62f595d38ffa991
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-23 12:25:31 -0600

    feat: getting posts -> passed

--- Scrum/Working/src/pages/threads/Threads.tsx
+++ Scrum/Working/src/pages/threads/Threads.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IonPage } from '@ionic/react';
 import './Threads.css'
 import PostField from '../../components/Threads/PostField';
 import { dummyPosts } from './dummyPosts';
 import Post from '../../components/Threads/Post';
+import { getThreadPosts } from '../../controller/ThreadController';
 
 const Threads: React.FC = () => {
+
+    const [posts, setPosts] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const fetchedPosts = await getThreadPosts();
+                setPosts(fetchedPosts);  // Save the fetched posts in state
+                setLoading(false);
+            } catch (err) {
+                setError('Failed to fetch posts');
+                setLoading(false);
+            }
+        };
+
+        fetchPosts();  // Call the function to fetch posts when component mounts
+    }, []);
+
     return (
         <IonPage>
             <div className='threads-space'>
                 <div className="threads-container">
                     <div className='thread-scroller'>
-                        <PostField/>
-                        {dummyPosts.map(post => (
-                        <Post 
-                        key={post.idthread}
-                        idthread={post.idthread}
-                        usuario={post.usuario}
-                        descripcion={post.descripcion}
-                        posttime={post.posttime}
-                        imagen={post.imagen}
-                        />
-                    ))}
+                        <PostField />
+                        
+                        {loading ? (
+                            <p>Loading posts...</p>
+                        ) : error ? (
+                            <p>{error}</p>
+                        ) : (
+                            posts.map(post => (
+                                <Post
+                                    key={post.idthreads}
+                                    idthread={post.idthreads}
+                                    usuario={post.usuario}
+                                    descripcion={post.descripcion}
+                                    posttime={post.posttime}
+                                    imagen={post.imagen}
+                                />
+                            ))
+                        )}
                     </div>
                     
 
