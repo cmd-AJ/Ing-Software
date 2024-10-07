ac9829776356eb567a42682200b6567addcd0353
wwIrvingww <mor22781@uvg.edu.gt>
2024-10-06 21:58:54 -0600

    feature: autoscroll to chat added

--- Scrum/Working/src/components/Chat/Chat.tsx
+++ Scrum/Working/src/components/Chat/Chat.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from 'react';
 import './chat.css';
 
 interface ChatBubbleProps {
@@ -12,35 +13,49 @@ interface Message {
     sender: 'me' | 'you';
 }
 
-const ChatBubble: React.FC<ChatBubbleProps>  = ({ message, time, sender }) => {
+const ChatBubble: React.FC<ChatBubbleProps> = ({ message, time, sender }) => {
     const formattedTime = (timeString: string) => {
-        const date = new Date(timeString); // Convertir la cadena de tiempo a un objeto Date
-        const hours = date.getHours(); // Obtener la hora en el horario local
-        const minutes = date.getMinutes().toString().padStart(2, '0'); // Asegurar que los minutos siempre tengan 2 dígitos
-        return `${hours}:${minutes}`;
-    };    
-    
+        const timePart = timeString.split('T')[1];
+        const [hour, minute] = timePart.split(':').slice(0, 2);
+        return `${hour}:${minute}`;
+    };
+
     return (
         <div className={`chat-bubble ${sender === 'me' ? 'me' : 'you'}`}>
             <div className="message-content">{message}</div>
             <div className="message-time">{formattedTime(time)}</div>
         </div>
     );
-}
+};
 
 interface ChatProps {
     messages: Message[];
-    onSendMessage: (newMessage: string) => void;
 }
 
 const Chat: React.FC<ChatProps> = ({ messages }) => {
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+
+    // Función para hacer scroll al final de la lista de mensajes
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    // Hacer scroll automáticamente cuando cambian los mensajes
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
     return (
         <div className="chat-container">
             {messages.map((msg, index) => (
                 <ChatBubble key={index} message={msg.message} time={msg.time} sender={msg.sender} />
             ))}
+            {/* Referencia al final del chat */}
+            <div ref={messagesEndRef} />
         </div>
     );
-}
+};
 
 export default Chat;
