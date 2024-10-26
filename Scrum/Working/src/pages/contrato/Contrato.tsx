47ea1d21e56c6c4519284cc2bbd233a4de144af7
josuemj <98498772+josuemj@users.noreply.github.com>
2024-10-26 16:39:53 -0600

    feat: description implemented

--- Scrum/Working/src/pages/contrato/Contrato.tsx
+++ Scrum/Working/src/pages/contrato/Contrato.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./contrato.module.css"; // CSS module for page-specific styles
 import TopBar from "./components/TopBar";
 import LandingSearchBar from "./components/LadingSearchBar";
+import { Padding } from "@mui/icons-material";
 
 const Contrato: React.FC = () => {
   const buttons = [
@@ -9,37 +10,40 @@ const Contrato: React.FC = () => {
     { label: "Iniciar sesion", onClick: () => {} },
   ];
 
-  const tempFontColor = {
-    color : 'black'
-  }
+  const text = "Quieres trabajar? ->";
   return (
     <div className={styles.contratoPage}>
       <TopBar buttons={buttons} />
       <div className={styles.content}>
-        
         {/* Section 1 */}
         <section className={styles.section1}>
-          
           <div className={styles.innerContainer}>
             <div className={styles.descriptionContainer}>
-              <p style={tempFontColor}>Encuentra El experto que tus amigos ya confian</p>
-              <p style={tempFontColor}>Conecta con profesionales de confianza recomenado por tu propia red</p>
-              <p style={tempFontColor}>Quieres trabajar?</p>
-              <LandingSearchBar onRequestChange={()=> {}} onSearch={() => {}}/>
+              <p className={styles.mainSlogan}>
+                Encuentra El experto Que tus amigos ya Confian.
+              </p>
+              <p className={styles.conectaCon}>
+                Conectate Con Profesionales De Confianza Recomendados Por Tu
+                Propia Red.
+              </p>
+              
+              <p className={styles.quieres} >{text}</p>
+              <LandingSearchBar
+                onRequestChange={() => {}}
+                onSearch={() => {}}
+              />
             </div>
 
             <div className={styles.productIdea}>
               <p>Nodes</p>
             </div>
-            
           </div>
         </section>
 
-        {/* Section 2 */}
-        <section className={styles.section2}>
+        {/* <section className={styles.section2}>
           <div className={styles.aboutDisplayer}></div>
           <div className={styles.productAssets}></div>
-        </section>
+        </section> */}
       </div>
     </div>
   );
