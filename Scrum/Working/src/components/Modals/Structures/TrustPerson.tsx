3d10a5a205c3e2ff8d3e98ba9903e5e74469a9f3
Jose-Prince <joseprince148@gmail.com>
2024-11-12 10:29:44 -0600

    Style: fix style for trusted people in profile

--- Scrum/Working/src/components/Modals/Structures/TrustPerson.tsx
+++ Scrum/Working/src/components/Modals/Structures/TrustPerson.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './TrustedStyle.css'
 import TextND from "../../Txt/TextND"
+import RateBar from "../../Miscellaneous/RateBar"
 
 interface ContainerProps {
     nombre: string
@@ -15,12 +16,12 @@ const TrustPerson: React.FC<ContainerProps> = ({nombre, apellido, rating, img, t
         <div className="persondatadisplay">
             <div style={{display: 'flex', height: '100%', width: '50%'}}>
                 <img src={img} alt="" style={{borderRadius: '50%', width: '15%', height: '20%', position: 'unset'}}/>
-                <div style={{display: 'flex', alignItems: 'center', marginLeft: '5px', flexDirection: 'column'}}>
+                <div style={{display: 'flex', alignItems: 'flex-start', marginLeft: '5px', flexDirection: 'column'}}>
                    <TextND text={`${nombre} ${apellido}`} hex="#000" size="medium"/>
                    <TextND text={tel} hex="#000" size="medium"/> 
                 </div>
             </div>
-            <TextND text={rating.toString()} hex="#000" size="medium"/>
+            <RateBar rating={rating}/>
         </div>
     )
 }
