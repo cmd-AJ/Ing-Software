5b109e9dc03c9da5c5d3d15b2d8e95dd71060021
cmd-AJ <Andrewlol774@gmail.com>
2024-11-28 14:27:33 -0600

    Updates

--- assets/md.transition-legacy-2oIz5Q0I.js
+++ assets/md.transition-legacy-2oIz5Q0I.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--FMZQ80S.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.A}],execute:function(){
+System.register(["./index-legacy-CdiLMSXv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.A}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
