61b0a6f650140054926991267bbb882deab8dfb3
cmd-AJ <Andrewlol774@gmail.com>
2024-11-28 14:49:56 -0600

    fixing base url

--- Scrum/Working/vite.config.ts
+++ Scrum/Working/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     legacy()
   ],
-  base: "https://cmd-aj.github.io/Ing-Software/",
+  base: "./Ing-Software/",
   test: {
     globals: true,
     environment: 'jsdom',
